b2d09529a7d6601d5250838475e5367a23ce87c7
bdqnhzxhlzj <2315434682@qq.com>
2019-02-16 09:43:13 +0800

    add demo62

---
+++ src/com/gittest0215/test/Demo62.java
@@ -0,0 +1,11 @@
+package com.gittest0215.test;
+/**
+* @author 作者 E-mail:
+* @version 创建时间：2019年2月16日 上午9:39:52
+* 类说明
+*/
+public class Demo62 {
+  public static void main(String[] args) {
+	System.out.println("---demo62----");
+}
+}
