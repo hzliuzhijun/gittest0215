37c89003ac18664c77e3fb6f676f347b3bf7dbef
hzliuzhijun <13989815478@163.com>
2019-02-16 08:57:30 +0800

    edit demo5

--- src/com/gittest0215/test/Demo5.java
+++ src/com/gittest0215/test/Demo5.java
@@ -7,12 +7,9 @@ package com.gittest0215.test;
 public class Demo5 {
    public static void main(String[] args) {
 	System.out.println("---demo5---");
-<<<<<<< HEAD
 	System.out.println("---demo5---");
 	System.out.println("---demo5---");
 	System.out.println("---demo5---");
 	System.out.println("---demo5---");
-=======
->>>>>>> branch 'master' of https://github.com/hzliuzhijun/gittest0215.git
 }
 }
