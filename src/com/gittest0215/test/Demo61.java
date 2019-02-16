2c0a5697dccf4386b5583580edfa30ab9b45c7e4
hzliuzhijun <13989815478@163.com>
2019-02-16 10:26:58 +0800

    回复

--- src/com/gittest0215/test/Demo61.java
+++ src/com/gittest0215/test/Demo61.java
@@ -3,8 +3,5 @@ package com.gittest0215.test;
 public class Demo61 {
 public static void main(String[] args) {
 	System.out.println("======demo 61======");
-	System.out.println("========demo 61==========");
-	System.out.println("========demo 61==========");
-	System.out.println("========demo 61==========");
 }
 }
